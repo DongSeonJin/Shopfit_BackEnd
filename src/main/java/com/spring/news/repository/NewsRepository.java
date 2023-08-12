3f6d5bf7bc6eb3cb8e64e9e1d0813ab0711ca052
lilylemonoh <lilylemonoh@naver.com>
2023-08-12 13:20:46 +0900

    OH: fix: repository 리턴타입 수정

--- src/main/java/com/spring/news/repository/NewsRepository.java
+++ src/main/java/com/spring/news/repository/NewsRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
 
-    List<NewsViewResponseDTO> findByTitleContaining(String keyword);
+    List<News> findByTitleContaining(String keyword);
 
 
 }
