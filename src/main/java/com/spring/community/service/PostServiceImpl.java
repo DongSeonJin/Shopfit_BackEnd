ccf153b123dd5a33753e403e22a88c296f279266
KwonA-Young <dkdud9817@naver.com>
2023-08-13 15:33:56 +0900

    KWON : add : controller, service 레이어 구현

--- src/main/java/com/spring/community/service/PostServiceImpl.java
+++ src/main/java/com/spring/community/service/PostServiceImpl.java
@@ -5,8 +5,10 @@ import com.spring.community.entity.Post;
 import com.spring.community.exception.PostIdNotFoundException;
 import com.spring.community.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,10 +17,10 @@ public class PostServiceImpl implements PostService{
     @Autowired
     PostRepository postRepository;
 
+    // 생성자 주입으로 변경
 
     @Override
     public Post getPostById(Long id) {
-
         return postRepository.findById(id).get();
     }
 
@@ -30,6 +32,10 @@ public class PostServiceImpl implements PostService{
 
     @Override
     public Post savePost(Post post) {
+        // post 저장 전, 시간정보와 조회수 설정
+        post.setCreatedAt(LocalDateTime.now());
+        post.setUpdatedAt(LocalDateTime.now());
+        post.setViewCount(0L); // post를 처음 저장할 때 조회수를 Long 타입으로 0으로 초기화
         return postRepository.save(post);
     }
 
@@ -45,6 +51,7 @@ public class PostServiceImpl implements PostService{
 
         post.setTitle(postUpdateDTO.getTitle());
         post.setContent(postUpdateDTO.getContent());
+        post.setUpdatedAt(LocalDateTime.now()); // 게시글 업데이트 된 시간 업데이트
         // 필요한 경우 다른 필드들도 업데이트
 
         postRepository.save(post);
