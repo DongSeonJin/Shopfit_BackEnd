4a151052fb54905be709937f90c91ae2c93b7928
DESKTOP-RU6BNO6\HANKUGIN <janggun21@naver.com>
2023-08-19 17:04:14 +0900

    HAN: add: Order 기능 구현

--- src/main/java/com/spring/shopping/entity/Order.java
+++ src/main/java/com/spring/shopping/entity/Order.java
@@ -1,99 +1,13 @@
 package com.spring.shopping.entity;
 
-import com.spring.user.entity.User;
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
-@Entity
-@Getter
-@ToString
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-@Table(name = "orders")
-public class Order {
+public class OrderDTO {
+    private Long userId;
+    private List<Long> productIds;
+    private Long totalPrice;
+    private String address;
+    private String phoneNumber;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "order_id")
-    private Long orderId;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id")
-    @OnDelete(action = OnDeleteAction.CASCADE) // 유저 삭제 시 주문도 삭제
-    private User user;
-
-    @Column(name = "total_price")
-    private Long totalPrice; // 총 금액
-
-    @Column(name = "delivery_date")
-    private LocalDateTime deliveryDate; // 배송일
-
-    @Column(name = "address", nullable = false)
-    private String address; // 주소
-
-    @Column(name = "phone_number", nullable = false)
-    private String phoneNumber; // 핸드폰 번호
-
-    @Column(name = "order_date")
-    private LocalDateTime orderDate; // 주문날짜
-
-    @Column(name = "order_status")
-    private String orderStatus; // 주문상태
-
-    @Column(name = "created_at")
-    @CreatedDate
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
-    @LastModifiedDate
-    private LocalDateTime updatedAt;
-
-    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<OrderProduct> orderProducts;
-
-    public List<OrderProduct> getOrderProducts() {
-        return orderProducts;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public void setTotalPrice(Long totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public void setDeliveryDate(LocalDateTime deliveryDate) {
-        this.deliveryDate = deliveryDate;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public void setOrderStatus(String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
-
-    public void setOrderProducts(List<OrderProduct> orderProducts) { this.orderProducts = orderProducts; }
+    // Getters, setters, constructors...
 }
