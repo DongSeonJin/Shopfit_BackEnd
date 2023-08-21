76b864ad8b7add1cd9703ccf316c326a15606198
bitcamp\bitcamp <janggun21@naver.com>
2023-08-21 12:44:51 +0900

    HAN: add: wishlist test 코드 작성

--- src/main/java/com/spring/shopping/DTO/WishlistDTO.java
+++ src/main/java/com/spring/shopping/DTO/WishlistDTO.java
@@ -1,9 +1,6 @@
 package com.spring.shopping.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
--- src/main/java/com/spring/shopping/service/WishlistServiceImpl.java
+++ src/main/java/com/spring/shopping/service/WishlistServiceImpl.java
@@ -1,44 +1,77 @@
 package com.spring.shopping.service;
 
+import com.spring.shopping.DTO.WishlistDTO;
+import com.spring.shopping.entity.Product;
+import com.spring.user.entity.User;
 import com.spring.shopping.entity.Wishlist;
+import com.spring.shopping.repository.ProductRepository;
+import com.spring.user.repository.UserRepository;
 import com.spring.shopping.repository.WishlistRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WishlistServiceImpl implements WishlistService {
 
     private final WishlistRepository wishlistRepository;
+    private final UserRepository userRepository;
+    private final ProductRepository productRepository;
 
     @Autowired
-    public WishlistServiceImpl(WishlistRepository wishlistRepository) {
+    public WishlistServiceImpl(WishlistRepository wishlistRepository, UserRepository userRepository, ProductRepository productRepository) {
         this.wishlistRepository = wishlistRepository;
+        this.userRepository = userRepository;
+        this.productRepository = productRepository;
     }
 
     @Override
-    public Wishlist addItemToWishlist(Wishlist wishlistItem) {
-        return wishlistRepository.save(wishlistItem);
+    public WishlistDTO addToWishlist(Long userId, Long productId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        Optional<Product> productOptional = productRepository.findById(productId);
+
+        if (userOptional.isPresent() && productOptional.isPresent()) {
+            User user = userOptional.get();
+            Product product = productOptional.get();
+
+            Wishlist wishlistItem = new Wishlist(user, product);
+            wishlistRepository.save(wishlistItem);
+
+            return convertToDTO(wishlistItem);
+        }
+
+        return null; // 사용자나 상품이 없을 경우 처리
     }
 
     @Override
-    public void removeItemFromWishlist(Long wishlistId) {
+    public void removeFromWishlist(Long wishlistId) {
         wishlistRepository.deleteById(wishlistId);
     }
 
     @Override
-    public List<Wishlist> getUserWishlist(Long userId) {
-        return wishlistRepository.findByUserId(userId);
+    public List<WishlistDTO> getUserWishlist(Long userId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            List<WishlistDTO> wishlistDTOList = new ArrayList<>();
+
+            List<Wishlist> wishlistItems = wishlistRepository.findByUser(user);
+            for (Wishlist wishlistItem : wishlistItems) {
+                wishlistDTOList.add(convertToDTO(wishlistItem));
+            }
+
+            return wishlistDTOList;
+        }
+
+        return new ArrayList<>();
     }
 
-    @Override
-    public List<Wishlist> getProductWishlist(Long productId) {
-        return wishlistRepository.findByProductId(productId);
-    }
-
-    @Override
-    public int getProductLikeCount(Long productId) {
-        return wishlistRepository.countByProductId(productId);
+    private WishlistDTO convertToDTO(Wishlist wishlist) {
+        return new WishlistDTO(wishlist.getWishlistId(), wishlist.getUser().getUserId(),
+                wishlist.getProduct().getProductId(), wishlist.getCreatedAt());
     }
 }
