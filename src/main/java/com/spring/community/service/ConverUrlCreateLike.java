c962115a3fb5d844a18789c0eaa6b058ff8e3460
Dongja <jds79793@gmail.com>
2023-09-03 00:04:08 +0900

    JIN: Feat: post별 likes 동적테이블 생성

--- src/main/java/com/spring/community/service/ConverUrlCreateLike.java
+++ src/main/java/com/spring/community/service/ConverUrlCreateLike.java
@@ -39,41 +39,41 @@ public class ConverUrlCreateLike {
 
                 // 추출한 닉네임별로 url저장.
                 for(int i = 0; i < nicknames.size(); i++){
-                    Statement stmt1 = conn.createStatement();
-                    ResultSet rt = stmt1.executeQuery("SELECT image_url1 FROM post WHERE nickname ='" + nicknames.get(i) +"'");  // Replace with your SQL query
-
-
-                    String url = null;
-                    if (rt.next()) {
-                        url = rt.getString(1);
-                        //System.out.println(url);
-                    }
-
-                    Statement stmt2 = conn.createStatement();
-                    ResultSet rt2 = stmt2.executeQuery("SELECT image_url2 FROM post WHERE nickname ='" + nicknames.get(i) +"'");  // Replace with your SQL query
-                    String imgurl2 = null;
-                    if(rt2.next()){
-                        imgurl2 = rt2.getString(1);
-                    }
-
-
-                    System.out.println("imageUrl1 변환성공, imageUrl1 :" + url);
-                    System.out.println("imageUrl2 변환성공, imageUrl2 :" + imgurl2);
-                    System.out.println("테이블 이름 :" + tableNames.get(i));
-                    System.out.println(i + "번째 변환성공======================================================");
-
-
-                    PreparedStatement stmt3 = conn.prepareStatement("UPDATE " + tableNames.get(i) + " SET image_url1=?, image_url2=?");
-                    stmt3.setString(1,url);
-                    stmt3.setString(2,imgurl2);
-
-                    int updatedRows = stmt3.executeUpdate();
+//                    Statement stmt1 = conn.createStatement();
+//                    ResultSet rt = stmt1.executeQuery("SELECT image_url1 FROM post WHERE nickname ='" + nicknames.get(i) +"'");  // Replace with your SQL query
+//
+//
+//                    String url = null;
+//                    if (rt.next()) {
+//                        url = rt.getString(1);
+//                        //System.out.println(url);
+//                    }
+//
+//                    Statement stmt2 = conn.createStatement();
+//                    ResultSet rt2 = stmt2.executeQuery("SELECT image_url2 FROM post WHERE nickname ='" + nicknames.get(i) +"'");  // Replace with your SQL query
+//                    String imgurl2 = null;
+//                    if(rt2.next()){
+//                        imgurl2 = rt2.getString(1);
+//                    }
+//
+//
+//                    System.out.println("imageUrl1 변환성공, imageUrl1 :" + url);
+//                    System.out.println("imageUrl2 변환성공, imageUrl2 :" + imgurl2);
+//                    System.out.println("테이블 이름 :" + tableNames.get(i));
+//                    System.out.println(i + "번째 변환성공======================================================");
+//
+//
+//                    PreparedStatement stmt3 = conn.prepareStatement("UPDATE " + tableNames.get(i) + " SET image_url1=?, image_url2=?");
+//                    stmt3.setString(1,url);
+//                    stmt3.setString(2,imgurl2);
+//
+//                    int updatedRows = stmt3.executeUpdate();
 
 //                    while (rt.next()){
 //                        url1.add(rt.getString(1));
 //                    }
 
-                    PreparedStatement stmt4 = conn.prepareStatement("CREATE TABLE likes_" + nicknames.get(i) + "(userId BIGINT, postId BIGINT, dynamic_post_id BIGINT," +
+                    PreparedStatement stmt4 = conn.prepareStatement("CREATE TABLE likes_" + nicknames.get(i) + "(like_id BIGINT PRIMARY KEY AUTO_INCREMENT, userId BIGINT, postId BIGINT, dynamic_post_id BIGINT," +
                             " createdAt DATETIME, FOREIGN KEY (user_id) REFERENCES users(user_id) " +
                             "ON DELETE CASCADE, FOREIGN KEY (post_id) REFERENCES post(post_id) ON DELETE CASCADE)");
 
