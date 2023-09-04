4dcbd39c69cc6596b77fd04b4d9738f89dcfc77c
lilylemonoh <lilylemonoh@naver.com>
2023-09-04 21:13:36 +0900

    OH: Feat: User 마이페이지 - 내 정보 보기 관련 기능 구현, 이미지 업로드 시 Image Optimizer 옵션 적용하는 코드 추가

--- src/main/java/com/spring/user/DTO/UserResponseDTO.java
+++ src/main/java/com/spring/user/DTO/UserResponseDTO.java
@@ -1,5 +1,10 @@
 package com.spring.user.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserResponseDTO {
     private Long userId;
     private String nickname;
