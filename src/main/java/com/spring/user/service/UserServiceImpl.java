3734992af9844b5d65267f84919cfefdab77a298
KwonA-Young <dkdud9817@naver.com>
2023-09-04 23:58:37 +0900

    KWON: Refactor: 필드주입 생성자주입으로 변경

--- src/main/java/com/spring/user/service/UserServiceImpl.java
+++ src/main/java/com/spring/user/service/UserServiceImpl.java
@@ -7,18 +7,24 @@ import com.spring.user.exception.UserIdNotFoundException;
 import com.spring.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService{
 
 
      private final UserRepository userRepository;
      private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+     @Autowired
+     public UserServiceImpl (UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+         this.userRepository = userRepository;
+         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+     }
 
 
     @Override
