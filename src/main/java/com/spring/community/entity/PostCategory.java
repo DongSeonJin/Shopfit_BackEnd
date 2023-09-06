988c196ed30beefb81683a3d2556926367eeb4b4
Dongja <jds79793@gmail.com>
2023-09-06 10:40:28 +0900

    JIN: docs: post Entity쪽 리펙토링계획 주석

--- src/main/java/com/spring/community/entity/PostCategory.java
+++ src/main/java/com/spring/community/entity/PostCategory.java
@@ -10,7 +10,7 @@ public class PostCategory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
+    @Column // OneToMany 붙일 예정, fetch = Lazy를 넣어 조인구문 시에만 호출되어 조회 성능 향상
     private Integer categoryId;
 
     @Column(nullable = false)
