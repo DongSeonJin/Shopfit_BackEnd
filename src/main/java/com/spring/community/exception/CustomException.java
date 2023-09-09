311b04a9f940347bb6272ece15119dfea0b15fbc
Dongja <129161266+jds79793@users.noreply.github.com>
2023-09-10 01:32:23 +0900

    JIN: Feat: 예외처리 custom할 enum 작성

--- src/main/java/com/spring/community/exception/CustomException.java
+++ src/main/java/com/spring/community/exception/CustomException.java
@@ -2,6 +2,8 @@ package com.spring.community.exception;
 
 
 public class CustomException extends RuntimeException{
+    // runtimeException 발생시 날려줄 메시지들을 enum에 작성해놓고,
+    // CustomException(현재 클래스) 에서 enum 객체를 생성
     private final ExceptionCode exceptionCode;
 
     public CustomException(ExceptionCode exceptionCode){
--- src/main/java/com/spring/community/exception/ExceptionCode.java
+++ src/main/java/com/spring/community/exception/ExceptionCode.java
@@ -4,9 +4,15 @@ import org.springframework.http.HttpStatus;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-public enum ExceptionCode {
+public enum ExceptionCode { // 예외 발생시, body에 실어 날려줄 상태, code, message 커스텀
+
+    // 예외 이름들 상수로 구분해서 적어줄 것.
     POST_NOT_FOUND(BAD_REQUEST, "ID_001", "해당되는 id의 게시물을 찾을 수 없습니다."),
     REPLY_NOT_FOUND(BAD_REQUEST, "ID_002", "해당되는 id의 댓글을 찾을 수 없습니다.");
+
+    // 1. status = 날려줄 상태코드(int형으로 변경 후 400으로 날려줘도 됨)
+    // 2. code = 해당 오류가 어느부분과 관련있는지 카테고리화 해주는 코드. 예외 원인 식별하기 편하기에 추가
+    // 3. message = 발생한 예외에 대한 설명.
 
     private final HttpStatus status;
     private final String code;
