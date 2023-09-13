7a56daf86aca6b49954644e89c220a21f3a9c8c2
lilylemonoh <lilylemonoh@naver.com>
2023-09-13 21:20:30 +0900

    OH: Test: 레디스 적용 쿠폰 발급 테스트코드 작성

--- src/main/java/com/spring/shopping/repository/CouponCountRepository.java
+++ src/main/java/com/spring/shopping/repository/CouponCountRepository.java
@@ -12,11 +12,15 @@ public class CouponCountRepository {
 
     public Long increment() {
         // Redis 데이터베이스에서 "couponcount"라는 키의 값을 증가시키는 메서드 - 카운터 구현
-        return redisTemplate.opsForValue().increment("couponcount"); // incr couponcount (Redis cli의 명령어와 유사하게 동작)
+        Long result = redisTemplate.opsForValue().increment("count", 1); //1을 증가시키도록 수정
+//        System.out.println("현재 count 값 :" + result);
+        return result;
+        // incr couponcount (Redis cli의 명령어와 유사하게 동작)
+
     }
 
     public void reset() {
-        redisTemplate.opsForValue().set("couponcount", "0"); // couponcount를 0으로 reset하기
+        redisTemplate.opsForValue().set("count", "0"); // couponcount를 0으로 reset하기
     }
 
 }
--- src/main/java/com/spring/shopping/service/CouponServiceImpl.java
+++ src/main/java/com/spring/shopping/service/CouponServiceImpl.java
@@ -4,6 +4,7 @@ import com.spring.shopping.DTO.CouponDTO;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -34,44 +35,46 @@ public class CouponServiceImpl implements CouponService {
     }
 
 
-    private final List<CouponDTO> couponList = new ArrayList<>();
+//    private final List<CouponDTO> couponList = new ArrayList<>();
 
-    @Override
-    public Optional<CouponDTO> getCouponById(Long couponId) {
-        return couponList.stream()
-                .filter(coupon -> coupon.getCouponId().equals(couponId))
-                .findFirst();
-    }
+//    @Override
+//    public Optional<CouponDTO> getCouponById(Long couponId) {
+//        return couponList.stream()
+//                .filter(coupon -> coupon.getCouponId().equals(couponId))
+//                .findFirst();
+//    }
 
-    @Override
-    public List<CouponDTO> getCouponsByUserId(Long userId) {
-        return couponList.stream()
-                .filter(coupon -> coupon.getUserId().equals(userId))
-                .collect(Collectors.toList());
-    }
+//    @Override
+//    public List<CouponDTO> getCouponsByUserId(Long userId) {
+//        return couponList.stream()
+//                .filter(coupon -> coupon.getUserId().equals(userId))
+//                .collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public List<CouponDTO> getExpiringCoupons(LocalDateTime dateTime) {
+//        return couponList.stream()
+//                .filter(coupon -> coupon.getValidTo().isBefore(dateTime))
+//                .collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public List<CouponDTO> getAllCoupons() {
+//        return couponList;
+//    }
+//
+//    @Override
+//    public void saveCoupon(CouponDTO couponDTO) {
+//        couponList.add(couponDTO);
+//    }
+//
+//    @Override
+//    public void deleteCoupon(Long couponId) {
+//        couponList.removeIf(coupon -> coupon.getCouponId().equals(couponId));
+//    }
 
-    @Override
-    public List<CouponDTO> getExpiringCoupons(LocalDateTime dateTime) {
-        return couponList.stream()
-                .filter(coupon -> coupon.getValidTo().isBefore(dateTime))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<CouponDTO> getAllCoupons() {
-        return couponList;
-    }
-
-    @Override
-    public void saveCoupon(CouponDTO couponDTO) {
-        couponList.add(couponDTO);
-    }
-
-    @Override
-    public void deleteCoupon(Long couponId) {
-        couponList.removeIf(coupon -> coupon.getCouponId().equals(couponId));
-    }
-
+    int testif = 0;
+    int testelse = 0;
 
     // 총 쿠폰 발급 개수를 체크하고, 쿠폰을 발급하는 메서드
     @Override
@@ -80,39 +83,33 @@ public class CouponServiceImpl implements CouponService {
 
         if(count > 100) { // 발급된 쿠폰이 100개가 넘으면 발급 방지
             return;
+        } else { // 발급된 쿠폰이 100개 이하라면 쿠폰 발급
+
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
+            String couponCode = "10000COUPON";  // 쿠폰코드
+
+            // 메인화면의 쿠폰 발급 쿠폰 생성 로직 - 10000원 할인쿠폰
+            Coupon coupon = Coupon.builder()
+                    .user(user)
+                    .couponCode(couponCode)
+                    .description("10,000원 할인 쿠폰")
+                    .discountValue(10000L)
+                    .validFrom(LocalDateTime.now())
+                    .validTo(LocalDateTime.now().plusDays(30))
+                    .build();
+
+            couponRepository.save(coupon);
         }
-        User user = getUserById(userId);
-        String couponCode = "10000COUPON";  // 쿠폰코드
-
-//        // 이미 해당 사용자에게 해당 쿠폰이 발급되었는지 확인 -- 아래에 메서드로 분리
-//        Optional<Coupon> existingCoupon = couponRepository.findByUser_UserIdAndCouponCode(userId, couponCode);
-//
-//        if(existingCoupon.isPresent()) {
-//            // 이미 발급된 쿠폰이 존재하면 중복 발급 방지
-//            return;
-//        }
-
-        // 중복이 아니라면 쿠폰 발급
-        Coupon coupon = createCouponForUser(user, couponCode);
-        couponRepository.save(coupon);
+    }
+    @Override
+    public void reset() {
+        couponCountRepository.reset();
     }
 
-    private User getUserById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found with ID: " + userId));
-    }
 
-    // 메인화면의 쿠폰 발급 쿠폰 생성 로직 - 10000원 할인쿠폰
-    private Coupon createCouponForUser(User user, String couponCode) {
-        return Coupon.builder()
-                .user(user)
-                .couponCode(couponCode)
-                .description("10,000원 할인 쿠폰")
-                .discountValue(10000L)
-                .validFrom(LocalDateTime.now())
-                .validTo(LocalDateTime.now().plusDays(30))
-                .build();
-    }
+
+
 
     // 어떤 유저가 이미 쿠폰을 가지고 있는지 확인하는 로직
     @Override
@@ -122,6 +119,20 @@ public class CouponServiceImpl implements CouponService {
         // 이미 쿠폰이 존재하는지 여부를 반환한다.
         return couponOptional.isPresent();
     }
+
+
+    // 유저 ID로 가지고 있는 쿠폰 조회
+    @Override
+    public List<CouponDTO> getCouponsByUserId(Long userId) {
+        LocalDateTime currentDate = LocalDateTime.now(); // 현재 날짜 가져오기
+
+        return couponRepository.findByUser_UserId(userId).stream()
+                .filter(coupon -> coupon.getValidTo().isAfter(currentDate)) // 유효기간이 아직 지나지 않은 쿠폰만 선택
+                .map(CouponDTO::new) // 엔터티를 DTO로 변환하여 반환
+                .collect(Collectors.toList());
+    }
+
+    // 쿠폰 사용
 
 
 }
