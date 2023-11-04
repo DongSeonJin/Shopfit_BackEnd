9ae0bd9e5e4c88ffdb2f48d813fb950a0d7c5daf
Dongja <129161266+jds79793@users.noreply.github.com>
2023-11-05 00:50:47 +0900

    JIN: Feat: AWS cloudDB 세팅

--- src/main/java/com/spring/community/controller/LikeController.java
+++ src/main/java/com/spring/community/controller/LikeController.java
@@ -3,7 +3,6 @@ package com.spring.community.controller;
 import com.spring.community.DTO.LikeRequestDTO;
 import com.spring.community.DTO.LikeResponseDTO;
 import com.spring.community.service.LikeService;
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,15 +17,15 @@ public class LikeController {
         this.likeService = likeService;
     }
 
-    @PostMapping("like/add") //좋아요 저장시에 id 중복검사 후 중복시 예외처리 추가 예정
+    @PostMapping("/like") //좋아요 저장시에 id 중복검사 후 중복시 예외처리 추가 예정
     public ResponseEntity<String> pushLike(@RequestBody LikeRequestDTO likeRequestDTO){
         likeService.saveLike(likeRequestDTO); //받아야 할 정보 : 글주인 nickname과 postId, 좋아요 누른사람 userId
         return ResponseEntity.ok("좋아요 누르기 성공");
     }
 
-    @PostMapping("like/delete")
-    public ResponseEntity<String> cancelLike(@RequestBody LikeRequestDTO likeRequestDTO){
-        likeService.deleteLike(likeRequestDTO);
+    @DeleteMapping("/like/{postId}/{userId}")
+    public ResponseEntity<String> cancelLike(@PathVariable Long postId, @PathVariable Long userId){
+        likeService.deleteLike(LikeRequestDTO.builder().postId(postId).userId(userId).build());
         return ResponseEntity.ok("좋아요 취소 성공");
     }
 
