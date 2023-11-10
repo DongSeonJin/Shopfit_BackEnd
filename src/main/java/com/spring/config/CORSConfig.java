b3f316d61169b7fd4c63ff31d6f10469a010fd50
Dongja <129161266+jds79793@users.noreply.github.com>
2023-11-10 20:08:38 +0900

    JIN: Feat: 프론트 배포서버 전역 CORS설정

---
+++ src/main/java/com/spring/config/CORSConfig.java
@@ -0,0 +1,18 @@
+package com.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://d33178k8dxtdif.cloudfront.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
