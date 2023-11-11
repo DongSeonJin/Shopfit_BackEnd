ef0e4899dea73378a2da9c700f3a5a48de4b3ab3
Dongja <129161266+jds79793@users.noreply.github.com>
2023-11-11 21:58:55 +0900

    JIN: Fix:  readme.md 배포주소 수정

--- src/main/java/com/spring/config/CORSConfig.java
+++ src/main/java/com/spring/config/CORSConfig.java
@@ -11,7 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("http://d33178k8dxtdif.cloudfront.net")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
