c9550ce46fb3ec0e7b3b88899f11aa64aa7430c0
Dongja <129161266+jds79793@users.noreply.github.com>
2024-03-24 21:25:21 +0900

    JIN: Feat: CORS 설정 추가

--- src/main/java/com/spring/config/CORSConfig.java
+++ src/main/java/com/spring/config/CORSConfig.java
@@ -11,6 +11,7 @@ public class CORSConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins("http://d33178k8dxtdif.cloudfront.net",
+                        "https://d33178k8dxtdif.cloudfront.net",
                         "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
